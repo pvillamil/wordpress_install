3c826d1972f7ae57691cc5198487dabf78eb1c83
Seth Swanson <sethswanson@gmail.com>
2020-01-09 22:10:32 -0500

    Removed unneeded mysql root pass db steps. changed how pkgs are installed. switched to tar from zip removing unneeded dep

--- wp_config.sh
+++ wp_config.sh
@@ -4,9 +4,6 @@
 set -eu
 
 mysql_secure_install() {
-    # perform mysql secure install steps
-    local root_pass=$1
-
     # remove anonymous users
     {
         anon_comm="DELETE FROM mysql.user WHERE User='';"
@@ -31,23 +28,14 @@ mysql_secure_install() {
         echo "Failed to remove the test db."
         echo "The script will continue but this should be addressed manually"
     }
-    # set root password to random password
-    {
-        pw_comm="UPDATE mysql.user SET authentication_string='$root_pass' WHERE User='root';FLUSH PRIVILEGES;"
-        mysql -u root -e "$pw_comm"
-    } || {
-        echo "Failed to update the mysql root password."
-        exit 1
-    }
 }
 
 mysql_configure() {
-    # $1=db name $2 =db root pass $3=db username $4=db user pass
+    # $1=db name $2=db username $3=db user pass
     # create wp_db
     local db_name=$1
-    local root_pass=$2
-    local db_user=$3
-    local db_user_pass=$4
+    local db_user=$2
+    local db_user_pass=$3
 
     # create the db
     {
@@ -75,50 +63,104 @@ mysql_configure() {
     }
 }
 
+php_config() {
+    local DB=$1
+    local USER=$2
+    local PASS=$3
+    local CONF_PATH=/var/www/wordpress/wp-config.php
+    local SAMPLE=/var/www/wordpress/wp-config-sample.php
+    local TMP_F=/tmp/wp-config.php
+    local KEY_NAMES=("AUTH_KEY" "SECURE_AUTH_KEY" "LOGGED_IN_KEY" "NONCE_KEY" "AUTH_SALT" "SECURE_AUTH_SALT" "LOGGED_IN_SALT" "NONCE_SALT")
+    
+    cp "$SAMPLE" "$CONF_PATH"
+
+    local keys=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+    
+    for name in "${KEY_NAMES[@]}"
+    do
+        key_line=$(grep -wn "$name" "$CONF_PATH" | cut -d ":" -f1)
+        new_key=$(echo "$keys" | grep -w "$name")
+        
+        # the large possibility fo special characters in the password makes using awk
+        # and sed very difficult so I opted to use head and tail to create a new file
+        
+        let "start = $key_line - 1"
+        let "end = $(wc -l $CONF_PATH | cut -d " " -f 1) - $key_line"
+        
+        head -n "$start" "$CONF_PATH" > "$TMP_F"
+        echo "$new_key" >> "$TMP_F"
+        tail -n "$end" "$CONF_PATH" >> "$TMP_F"
+        
+        mv "$TMP_F" "$CONF_PATH"
+    done
+
+    sed -i 's/database_name_here/'"$DB"'/g' "$CONF_PATH"
+    sed -i 's/username_here/'"$USER"'/g' "$CONF_PATH"
+    sed -i 's/password_here/'"$PASS"'/g' "$CONF_PATH"
+
+    if [[ ! $(grep "FS_METHOD" $CONF_PATH) ]]
+    then
+        echo "define('FS_METHOD', 'direct');" >> $CONF_PATH
+    fi
+}
+
+install_pkg() {
+    local PKG=$1
+
+    if [[ ! $(dpkg -l "$PKG") ]]; then
+        {
+            echo "Installing $PKG"
+            apt-get install -y "$PKG" 2> /dev/null
+        } || {
+            echo "Failed to install $PKG"
+            exit 1
+        }
+    fi
+}
+
 WWW_ROOT=/var/www/
 WP_ROOT="${WWW_ROOT}wordpress/"
 DBUSER=wpUser
 
-# install unzip to make sure wp can be extracted
-if [[ ! $(dpkg -l unzip) ]]; then
-    {
-        apt-get install -y unzip
-    } || {
-        echo "Failed to install unzip"
-        exit 1
-    }
-fi
+PACKAGES=("php" "php-curl" "php-gd" "php-intl" "php-mbstring" "php-soap"
+    "php-xml" "php-xmlrpc" "php-zip" "php-fpm" "php-mysql" "mysql-server"
+    "nginx")
+
+for pkg in "${PACKAGES[@]}"
+do
+    install_pkg "$pkg" 
+done
 
 # check if php is installed and install if it is not
-if [[ ! $(dpkg -l php) ]]; then
-    {
-        apt-get install -y php php-curl php-gd php-intl php-mbstring php-soap
-        php-xml php-xmlrpc php-zip
-    } || {
-        echo "Failed to install PHP"
-        exit 1
-    }
-fi
-
-# check if mysql is installed and install if it is not
-if [[ ! $(dpkg -l mysql-server) ]]; then
-    {
-        apt-get install -y mysql-server
-    } || {
-        echo "Failed to install mysql"
-        exit 1
-    }
-fi
-
-# check if nginx is installed and install if it is not
-if [[ ! $(dpkg -l nginx) ]]; then
-    {
-        apt-get install -y nginx
-    } || {
-        echo "Failed to install nginx"
-        exit 1
-    }
-fi
+#if [[ ! $(dpkg -l php) ]]; then
+#    {
+#        apt-get install -y php php-curl php-gd php-intl php-mbstring php-soap \
+#        php-xml php-xmlrpc php-zip php-fpm php-mysql
+#    } || {
+#        echo "Failed to install PHP"
+#        exit 1
+#    }
+#fi
+#
+## check if mysql is installed and install if it is not
+#if [[ ! $(dpkg -l mysql-server) ]]; then
+#    {
+#        apt-get install -y mysql-server
+#    } || {
+#        echo "Failed to install mysql"
+#        exit 1
+#    }
+#fi
+#
+## check if nginx is installed and install if it is not
+#if [[ ! $(dpkg -l nginx) ]]; then
+#    {
+#        apt-get install -y nginx
+#    } || {
+#        echo "Failed to install nginx"
+#        exit 1
+#    }
+#fi
 
 # get domain name
 echo "Please enter the domain name: "
@@ -147,31 +189,28 @@ sed -i 's/domain.tld/'"$domain"'/g' /etc/nginx/conf.d/wordpress.conf
 
 # download latest wordpress
 { 
-    curl -LO http://wordpress.org/latest.zip 
+    cd /tmp
+    curl -LO http://wordpress.org/latest.tar.gz
 } || { 
     echo "Failed to download the latest wordpress"
     exit 1
 }
 
-# unzip latest
+# extract latest
 {
     if [[ ! -d "${WWW_ROOT}wordpress" ]]
     then    
-        unzip latest.zip -d "$WWW_ROOT"
+        tar -C ${WWW_ROOT} -xvzf latest.tar.gz
     else
         echo "${WWW_ROOT}wordpress already exists."
     fi
 } || {
-    echo "Failed to extract the wordpress zip."
+    echo "Failed to extract the wordpress archive."
     exit 1
 }
 
 ### db setup
-# generate random root db password
-# there are more random options but this is probably good enough for this use
-# case
-DBPASS=$(date | md5sum | awk '{print $1}')
-echo "PLEASE NOTE!\nThe DB root password is: $DBPASS"
+#echo "PLEASE NOTE!\nThe DB root password is: $DBPASS"
 # start service
 { 
     systemctl start mysql
@@ -187,20 +226,15 @@ echo "PLEASE NOTE!\nThe DB root password is: $DBPASS"
     echo "The script will continue but this should be addressed manually."
 }
 ## mysql secure install
-DBNAME="$(echo "$domain""_db")"
-mysql_secure_install "$DBPASS"
+DBNAME="${domain}_db"
 # create password for db user
 DBUSERPASS=$(date | md5sum | awk '{print $1}')
 echo -e "PLEASE NOTE!\n The pw for the db user wp_db_user is: $DBUSERPASS"
 ## configure the db
-mysql_configure "$DBNAME" "$DBPASS" "$DBUSER" "$DBUSERPASS"
+mysql_configure "$DBNAME" "$DBUSER" "$DBUSERPASS"
 
 ## create wp-config.php
-cp "${WP_ROOT}wp-config-sample.php" "${WP_ROOT}wp-config.php"
-sed -i 's/database_name_here/'"$DBNAME"'/g' "${WP_ROOT}wp-config.php"
-sed -i 's/username_here/'"$DBUSER"'/g' "${WP_ROOT}wp-config.php"
-sed -i 's/password_here/'"$DBUSERPASS"'/g' "${WP_ROOT}wp-config.php"
-
+php_config
 
 ## make sure no apache instances are running
 systemctl stop apache2
